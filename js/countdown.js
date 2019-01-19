cbc9ed2f3a82d875e125a944b60d3d8fece24167
Dongmin Kim <psycholic4@gmail.com>
2019-01-19 22:45:50 +0900

    contents fixed

--- js/countdown.js
+++ js/countdown.js
@@ -1,28 +1,34 @@
 
 var marriageDate = new Date('Mar 2, 2019 11:00:00').getTime();
+var countDownStartTime = new Date().getTime();
+var oneDay = 1000 * 60 * 60 * 24;
+var oneHour = 1000 * 60 * 60;
+var oneMinute = 1000 * 60;
 
 printCountDown()
 
-var x = setInterval(function() {
+var interval = setInterval(function() {
   if (!printCountDown()) {
-    clearInterval(x);
+    clearInterval(interval);
   }
 }, 1000);
 
 function printCountDown() {
   var now = new Date().getTime();
-  var diff = marriageDate - now;
+  var diff = Math.abs(marriageDate - now);
+  var married = marriageDate - now <= 0
 
   // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(diff / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((diff % (1000 * 60)) / 1000);
+  var days = Math.floor(diff / oneDay);
+  var hours = Math.floor((diff % oneDay) / oneHour);
+  var minutes = Math.floor((diff % oneHour) / oneMinute);
+  var seconds = Math.floor((diff % oneMinute) / 1000);
 
-  document.getElementById('countdown-message').innerHTML = days + 'd ' + hours + 'h ' + minutes + 'm ' + seconds + 's ';
-
-  if (diff < 0) {
-    document.getElementById('countdown-message').innerHTML = 'We are merried!';
+  if (married) {
+    document.getElementById('countdown-message').innerHTML = '우리가 결혼한 지<br>' + days + '일 ' + hours + '시간 ' + minutes + '분 ' + seconds + '초';
+  } else {
+    document.getElementById('countdown-message').innerHTML = '우리의 결혼까지<br>' + days + '일 ' + hours + '시간 ' + minutes + '분 ' + seconds + '초';
   }
-  return diff >= 0;
+  // 1시간 동안만 실행되도록
+  return now - countDownStartTime < oneHour;
 }
